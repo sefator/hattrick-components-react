9c552654917c4e219ef560fd3c6189c3fc36c5d8
Jiri Gabrys <jgabrys@chemaxon.com>
2021-03-19 18:29:53 +0100

    Generated bindings from stencil

---
+++ src/components.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@hattrickltd/hattrick-components';
+
+
+
+export const HattrickAvatar = /*@__PURE__*/createReactComponent<JSX.HattrickAvatar, HTMLHattrickAvatarElement>('hattrick-avatar');
+export const HattrickBar = /*@__PURE__*/createReactComponent<JSX.HattrickBar, HTMLHattrickBarElement>('hattrick-bar');
+export const HattrickFlip = /*@__PURE__*/createReactComponent<JSX.HattrickFlip, HTMLHattrickFlipElement>('hattrick-flip');
+export const HattrickMatchClock = /*@__PURE__*/createReactComponent<JSX.HattrickMatchClock, HTMLHattrickMatchClockElement>('hattrick-match-clock');
+export const HattrickMl = /*@__PURE__*/createReactComponent<JSX.HattrickMl, HTMLHattrickMlElement>('hattrick-ml');
+export const HattrickPicture = /*@__PURE__*/createReactComponent<JSX.HattrickPicture, HTMLHattrickPictureElement>('hattrick-picture');
+export const HattrickPlayoffTree = /*@__PURE__*/createReactComponent<JSX.HattrickPlayoffTree, HTMLHattrickPlayoffTreeElement>('hattrick-playoff-tree');
+export const HattrickProgressArc = /*@__PURE__*/createReactComponent<JSX.HattrickProgressArc, HTMLHattrickProgressArcElement>('hattrick-progress-arc');
+export const HattrickRange = /*@__PURE__*/createReactComponent<JSX.HattrickRange, HTMLHattrickRangeElement>('hattrick-range');
+export const HattrickRating = /*@__PURE__*/createReactComponent<JSX.HattrickRating, HTMLHattrickRatingElement>('hattrick-rating');
+export const HattrickTimer = /*@__PURE__*/createReactComponent<JSX.HattrickTimer, HTMLHattrickTimerElement>('hattrick-timer');
+export const HattrickTooltip = /*@__PURE__*/createReactComponent<JSX.HattrickTooltip, HTMLHattrickTooltipElement>('hattrick-tooltip');
